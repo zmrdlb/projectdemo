4e236f9671fe7c1f486850a87d99d23b18f361c6
zhangmingrui <592044573@qq.com>
2016-12-30 20:23:05 +0800

    model使用修改

--- src/js/common/model.js
+++ src/js/common/model.js
@@ -16,10 +16,10 @@ define(['libio/ioconfig','libio/interio','common/config'],function($ioconfig,$in
 	/**
 	 * 所有接口的io业务错误统一处理
 	 */
-	$ioconfig.error.filter = function(result) {
+	$ioconfig.fail.filter = function(result) {
 		return result.code != 'A0001';
 	};
-	$ioconfig.ioargs.fail = function(result){
+	$ioconfig.iocall.fail = function(result){
 		alert(result.errmsg || '网络错误');
 	};
 
@@ -28,12 +28,6 @@ define(['libio/ioconfig','libio/interio','common/config'],function($ioconfig,$in
 	 * 数据接口配置
 	 */
 	var basehost = Config.modelorgin;
-	$ioconfig.setTrans([
-		//没此接口，只是给一个url中需要参数的使用说明
-		{name: 'main', args: {url: basehost+'/tracer/main/trace/{id}',method:'GET'}},
-		//获取用户信息
-        {name: 'userinfo', args: {url: basehost+'/userinfo',method:'GET'}}
-    ]);
 
     /**
      * url格式化
@@ -46,34 +40,44 @@ define(['libio/ioconfig','libio/interio','common/config'],function($ioconfig,$in
             return isencode? encodeURIComponent(data[key]): data[key];
         });
     }
-    /**
-     * 请求数据层model
-     * @param {Object} *opt 传入的参数同$ioconfig.ioargs格式一致，一般是：
-         * {
-         * 	   data: {}
-         *     success
-         *     //以下已经有统一处理，如果想覆盖则自行传入
-         *     error
-         *     fail
-         * }
-       @param {Object} urlData 针对url里面有{替换参数名}的情况，传入的键值对应数据
-     */
-    return {
+	/**
+	 * 请求数据层model
+	 * @param {Object} ioargs 传入的参数同$ioconfig.ioargs格式一致，一般是：
+		 * {
+		 * 	   data: {}
+		 * }
+	   @param {JSON} *iocall 传入的参数同$ioconfig.iocall格式一致，一般是：
+	   * {
+	   *     success
+	   *     complete
+	   *     //以下已经有统一处理，如果想覆盖则自行传入
+	   *     error
+	   *     fail
+	   * }
+	   @param {Object} urlData 针对url里面有{替换参数名}的情况，传入的键值对应数据
+	 */
+	return {
 		/**
-         * 获取项目文件目录结构
-         * urlData: {
-         *     id
-         * }
-         */
-        main: function(opt,urlData){
-            opt.url = buildUrl($ioconfig.getTrans('main').url,urlData,true);
-            $interio.transRequest('main',opt);
-        },
+		 * 获取项目文件目录结构
+		 * urlData: {
+		 *     id
+		 * }
+		 */
+		main: function(ioargs,iocall,urlData){
+			var _url = buildUrl(basehost+'/tracer/main/trace/{id}',urlData,true);
+			$interio.transRequest($.extend({
+				url: _url,
+				method:'GET'
+			},ioargs),iocall);
+		},
 		/**
-         * 获取当前用户信息
-         */
-        userinfo: function(opt){
-            $interio.transRequest('userinfo',opt);
-        }
-    };
+		 * 列表数据
+		 */
+		listdata: function(ioargs,iocall){
+			$interio.transRequest($.extend({
+				url: basehost+'/listdata',
+				method:'POST'
+			},ioargs),iocall);
+		}
+	};
 });
